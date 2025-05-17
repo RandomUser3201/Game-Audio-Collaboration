ae553338c870580b9040d53814ffcf40a5ae24b7
Zeesh <Mohammed.Zeeshan3@mail.bcu.ac.uk>
2025-05-17 17:30:44 +0100

    (+) Bridge Guard Warnings Implemented
    Added 3 audio events in fmod, connected to unity.
    Depending on how close the player is to the guard - they will activate different dialogue. Added a timer, to stop crashing and avoid and overlaps.

---
+++ Assets/Scripts/GuardWarning.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using System.Diagnostics.Tracing;
+
+public class GuardWarning : MonoBehaviour
+{
+    public Transform Player;
+
+    [Header("Distances")]
+    public float WarningDistance1 = 20f;
+    public float WarningDistance2 = 10f;
+    public float WarningDistance3 = 5f;
+
+    [Header("Events")]
+    public EventReference WarningEvent1;
+    public EventReference WarningEvent2;
+    public EventReference WarningEvent3;
+    
+    [Header("Cooldown")]
+    public float CooldownTime1 = 10f;
+    public float CooldownTime2 = 10f;
+    public float CooldownTime3 = 10f;
+
+    private float _timer1 = 0f;
+    private float _timer2 = 0f;
+    private float _timer3 = 0f;
+
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(Player.position, transform.position);
+
+        if (_timer1 > 0)
+        {
+            _timer1 -= Time.deltaTime;
+        }
+        if (_timer2 > 0)
+        {
+            _timer2 -= Time.deltaTime;
+        }
+        if (_timer3 > 0)
+        {
+            _timer3 -= Time.deltaTime;
+        }
+
+        if (distance <= WarningDistance1 && distance >= WarningDistance2 && _timer1 <= 0f)
+        {
+            RuntimeManager.PlayOneShot(WarningEvent1, transform.position);
+            _timer1 = CooldownTime1;
+        }
+
+        if (distance <= WarningDistance2 && distance >= WarningDistance3 && _timer2 <= 0f)
+        {
+            RuntimeManager.PlayOneShot(WarningEvent2, transform.position);
+            _timer2 = CooldownTime2;
+        }
+
+        if (distance < WarningDistance3 && _timer3 <= 0f)
+        {
+            RuntimeManager.PlayOneShot(WarningEvent3, transform.position);
+            _timer3 = CooldownTime3;
+        }
+    }
+}
